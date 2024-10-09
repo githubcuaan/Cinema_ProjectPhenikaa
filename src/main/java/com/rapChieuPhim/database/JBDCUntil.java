6426ff0d4da84798de6a9ed6604e1ca0aa942f8f
Dinh An <dinhan.0321@gmail.com>
2024-10-09 12:08:14 +0700

    init OOP RapChieuPhim project

---
+++ src/main/java/com/rapChieuPhim/database/JBDCUntil.java
@@ -0,0 +1,43 @@
+
+package com.rapChieuPhim.database;
+
+/**
+ *
+ * @author DinhAn
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JBDCUntil {
+    public static Connection getConnection()
+    {
+        Connection c = null;
+        
+        try {
+            //tạo thông số
+            String url = "jdbc:mysql://localhost:3306/demove";
+            String user = "root";
+            String password = "Dinhan.0321@";
+            
+            //đăng kí driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //tạo kết nối
+            c = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+    
+    public static void closeConnection(Connection c)
+    {
+        try {
+            if (c != null) {
+                c.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
